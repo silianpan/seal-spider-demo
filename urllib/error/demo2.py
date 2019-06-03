7db800c8fbc36eb9aaf8c8d6258bcc9c8de517e2
liupan <1206284818@qq.com>
2019-06-04 00:34:46 +0800

    error模块

--- urllib/error/demo2.py
+++ urllib/error/demo2.py
@@ -1,4 +1,6 @@
 from urllib import request,error
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 try:
     response = request.urlopen('http://cuiqingcai.com/index.htm')
 except error.HTTPError as e:
