ba5e89843d2dbb53c91a6a99e41468dd14e998ae
liupan <1206284818@qq.com>
2019-06-10 20:54:26 +0800

    robots.txt解析ssl验证，HTTP 403 forbidden问题解决

--- urllib/parser/demo1.py
+++ urllib/parser/demo1.py
@@ -1,7 +1,9 @@
 from urllib.robotparser import RobotFileParser
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 rp = RobotFileParser()
-rp.set_url('http://www.jianshu.com/robots.txt')
+rp.set_url('https://www.jianshu.com/robots.txt')
 rp.read()
-print(rp.can_fetch('*', 'http://www.jianshu.com/p/b67554025d7d'))
+print(rp.can_fetch('*', 'https://www.jianshu.com/p/b67554025d7d'))
 print(rp.can_fetch('*', "http://www.jianshu.com/search?q=python&page=1&type=collections"))
--- urllib/parser/demo2.py
+++ urllib/parser/demo2.py
@@ -1,7 +1,11 @@
 from urllib.robotparser import RobotFileParser
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 rp = RobotFileParser()
-rp.parse(urlopen('http://www.jianshu.com/robots.txt').read().decode('utf-8').split('\n'))
-print(rp.can_fetch('*', 'http://www.jianshu.com/p/b67554025d7d'))
-print(rp.can_fetch('*', "http://www.jianshu.com/search?q=python&page=1&type=collections"))
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:67.0) Gecko/20100101 Firefox/67.0'}
+req = Request(url='https://www.jianshu.com/robots.txt', headers=headers)
+rp.parse(urlopen(req).read().decode('utf-8').split('\n'))
+print(rp.can_fetch('*', 'https://www.jianshu.com/p/b67554025d7d'))
+print(rp.can_fetch('*', "https://www.jianshu.com/search?q=python&page=1&type=collections"))
