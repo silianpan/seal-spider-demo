7c09ba35b78197b78998c7964f6ed7260f4f01c9
liupan <1206284818@qq.com>
2020-01-27 21:31:50 +0800

    北大法宝以前版本爬虫

---
+++ pyspider/pkulaw2.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Created on 2020-01-14 15:19:11
+# Project: pkulaw
+
+import re
+
+import pymysql
+from pyspider.libs.base_handler import *
+
+# 正则表达式
+pattern_article = re.compile('^https://www.pkulaw.com/chl/\w{20}.html$')
+
+
+class Handler(BaseHandler):
+    crawl_config = {
+        'headers': {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
+            'Referer': 'https://www.pkulaw.com/',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    }
+
+    @every(minutes=10 * 24 * 60)
+    def on_start(self):
+        # 第一页请求抓取
+        self.crawl('https://www.pkulaw.com/law/search/RecordSearch', method='POST', data={
+            'Menu': 'law',
+            'SearchKeywordType': 'DefaultSearch',
+            'MatchType': 'Exact',
+            'RangeType': 'Piece',
+            'Library': 'chl',
+            'ClassFlag': 'chl',
+            'QueryOnClick': 'False',
+            'AfterSearch': 'False',
+            'IsSynonymSearch': 'true',
+            'IsAdv': 'False',
+            'ClassCodeKey': ',XA01,,,,',
+            'GroupByIndex': 0,
+            'OrderByIndex': 0,
+            'ShowType': 'Default',
+            'Pager.PageIndex': 0,
+            'RecordShowType': 'List',
+            'Pager.PageSize': 100,
+            'isEng': 'chinese',
+            'X-Requested-With': 'XMLHttpRequest'
+        }, callback=self.index_page)
+
+    @config(age=5 * 24 * 60 * 60)
+    def index_page(self, response):
+        pages = response.doc('ul.pagination-sm > li.disabled > label').text()
+        if pages is not None and len(pages.strip()) != 0 and '/' in pages:
+            pages_list = pages.split('/')
+            tmp = pages_list[0]
+            tmp_list = tmp.split(' ')
+            current_index = int(tmp_list[1]) + 1
+            page_size = int(pages_list[1])
+            if 1 < current_index <= page_size:
+                # 回调index_page
+                self.crawl('https://www.pkulaw.com/law/search/RecordSearch', method='POST', data={
+                    'Menu': 'law',
+                    'SearchKeywordType': 'DefaultSearch',
+                    'MatchType': 'Exact',
+                    'RangeType': 'Piece',
+                    'Library': 'chl',
+                    'ClassFlag': 'chl',
+                    'QueryOnClick': 'False',
+                    'AfterSearch': 'False',
+                    'IsSynonymSearch': 'true',
+                    'IsAdv': 'False',
+                    'ClassCodeKey': ',XA01,,,,',
+                    'GroupByIndex': 0,
+                    'OrderByIndex': 0,
+                    'ShowType': 'Default',
+                    'Pager.PageIndex': current_index - 1,
+                    'RecordShowType': 'List',
+                    'Pager.PageSize': 100,
+                    'isEng': 'chinese',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'OldPageIndex': current_index - 2
+                }, callback=self.index_page)
+        # 逐条处理
+        self.item_page(response)
+
+    @config(priority=2)
+    def item_page(self, response):
+        for each in response.doc('a[href^="http"]').items():
+            if re.match(pattern_article, each.attr.href):
+                self.crawl(each.attr.href, callback=self.detail_page)
+
+    @config(priority=3)
+    def detail_page(self, response):
+        # 详细处理
+        title = response.doc('.content > .title')
+        title.children().remove()
+        li_list = response.doc('.content > .fields > ul > li').items()
+        ret = {}
+        for li in li_list:
+            strong = li('strong').text()
+            if strong is not None:
+                if u'发布部门' in strong.strip():
+                    cont = li('span').attr('title')
+                    if cont is not None and len(cont.strip()) != 0:
+                        ret['pub_dept'] = cont
+                    else:
+                        ret['pub_dept'] = li.attr('title')
+                elif u'发文字号' in strong.strip():
+                    ret['pub_no'] = li.attr('title')
+                elif u'发布日期' in strong.strip():
+                    # cont = li.attr('title')
+                    # if cont is None or len(cont.strip()) == 0:
+                    #     li.children().remove()
+                    #     cont = li.text()
+                    ret['pub_date'] = li.attr('title')
+                elif u'实施日期' in strong.strip():
+                    # cont = li.attr('title')
+                    # if cont is None or len(cont.strip()) == 0:
+                    #     li.children().remove()
+                    #     cont = li.text()
+                    ret['impl_date'] = li.attr('title')
+                elif u'时效性' in strong.strip():
+                    cont = li('span').attr('title')
+                    if cont is not None and len(cont.strip()) != 0:
+                        ret['time_valid'] = cont
+                    else:
+                        ret['time_valid'] = li.attr('title')
+                elif u'效力级别' in strong.strip():
+                    ret['force_level'] = li('span').attr('title')
+                    # if cont is not None and len(cont.strip()) != 0:
+                    #     ret['force_level'] = cont
+                    # else:
+                    #     ret['force_level'] = li.attr('title')
+                elif u'法规类别' in strong.strip():
+                    ret['law_type'] = li('span').attr('title')
+                    # if cont is not None and len(cont.strip()) != 0:
+                    #     ret['law_type'] = cont
+                    # else:
+                    #     ret['law_type'] = li.attr('title')
+                elif u'类别' in strong.strip():
+                    ret['type'] = li('a').text()
+                elif u'截止日期' in strong.strip():
+                    li.children().remove()
+                    cont = li.text()
+                    # if cont is None or len(cont.strip()) == 0:
+                    #     cont = li.attr('title')
+                    ret['deadline'] = cont
+
+        ret['url'] = response.url
+        ret['title'] = title.text().strip()
+        ret['content'] = response.doc('.content > .fulltext').html().strip()
+
+        # 保存mysql
+        if u'现行有效' in ret['time_valid'] or u'尚未生效' in ret['time_valid']:
+            if 'deadline' not in ret:
+                ret['deadline'] = ''
+            if 'type' not in ret:
+                ret['type'] = ''
+            if 'pub_no' not in ret:
+                ret['pub_no'] = ''
+            self.save_to_mysql(
+                (ret['title'], ret['pub_dept'], ret['pub_no'], ret['pub_date'], ret['law_type'], ret['force_level'],
+                 ret['time_valid'], ret['impl_date'], ret['content'], ret['url'], ret['type'], ret['deadline']))
+        return ret
+
+    # 保存到mysql
+    def save_to_mysql(self, params):
+        db = pymysql.connect(host='localhost', user='root', password='Asdf@123', port=3306, db='pkulaw')
+        cursor = db.cursor()
+        sql = 'INSERT INTO law(title, pub_dept, pub_no, pub_date, law_type, force_level, time_valid, impl_date, content, url, type, deadline) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        try:
+            cursor.execute(sql, params)
+            db.commit()
+        except:
+            db.rollback()
+        # cursor.execute(sql, params)
+        # db.commit()
+        cursor.close()
+        db.close()
--- selenium/baidu_logo.py
+++ selenium/baidu_logo.py
@@ -5,3 +5,83 @@
 # @Site    : 
 # @File    : baidu_logo.py
 # @Software: PyCharm
+
+# -*- coding: utf-8 -*-
+
+import math
+from base64 import b64decode
+from io import StringIO
+
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+# from wand.image import Image
+
+
+def get_element_screenshot(element: WebElement) -> bytes:
+    driver = element._parent
+    ActionChains(driver).move_to_element(element).perform()  # focus
+    src_base64 = driver.get_screenshot_as_base64()
+    scr_png = b64decode(src_base64)
+    scr_img = Image(blob=scr_png)
+
+    x = element.location["x"]
+    y = element.location["y"]
+    w = element.size["width"]
+    h = element.size["height"]
+    scr_img.crop(
+        left=math.floor(x),
+        top=math.floor(y),
+        width=math.ceil(w),
+        height=math.ceil(h),
+    )
+    return scr_img.make_blob()
+
+
+print("开始爬取")
+# 创建chrome参数对象
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
+# options.add_argument('--window-size=1920,1080')  # 指定浏览器窗口大小
+options.add_argument('--start-maximized')  # 浏览器窗口最大化
+options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
+# options.add_argument('--blink-settings=imagesEnabled=false')  # 不加载图片,加快访问速度
+options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+options.add_argument('test-type')
+options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors",
+                                                    "enable-automation"])  # 此步骤很重要，设置为开发者模式，防止被各大网站识别出来使用了Selenium
+# options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})  # 不加载图片,加快访问速度
+
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+# driver.set_window_size(1366, 768)
+# driver.viewportSize = {'width': 1366, 'height': 768}
+driver.get('http://www.baidu.com')
+print(driver.title)
+baidu_img = WebDriverWait(driver, 20).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, '.index-logo-srcnew'))
+)
+# get_element_screenshot(baidu_img)
+img = driver.get_screenshot_as_png()  # 对整个浏览器页面进行截图
+print(baidu_img.size)
+# x, y = baidu_img.location.values()
+x = baidu_img.location_once_scrolled_into_view['x']
+y = baidu_img.location_once_scrolled_into_view['y']
+# x = 580
+# y = 160
+right = x + baidu_img.size['width']
+bottom = y + baidu_img.size['height']
+
+# im = Image.open('screenshot.png')
+im = Image.open(StringIO(img))
+# im.resize((1600, 1200))
+print((x, y, right, bottom))
+im = im.crop((x, y, right, bottom))  # 对浏览器截图进行裁剪
+im.save('baidu.png')
+# driver.quit()
+print("爬取完成")
