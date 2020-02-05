254e3e28f0b003e27183d8418dbe228057afa0b7
liupan <1206284818@qq.com>
2020-02-05 14:03:25 +0800

    pkulaw scrapy

---
+++ pkulaw/pkulaw/pkulaw/middlewares.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+import logging
+
+import requests
+from fake_useragent import UserAgent
+from scrapy import signals
+from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
+
+
+class PkulawSpiderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the spider middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        # Called for each response that goes through the spider
+        # middleware and into the spider.
+
+        # Should return None or raise an exception.
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        # Called with the results returned from the Spider, after
+        # it has processed the response.
+
+        # Must return an iterable of Request, dict or Item objects.
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+
+        # Should return either None or an iterable of Request, dict
+        # or Item objects.
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        # Called with the start requests of the spider, and works
+        # similarly to the process_spider_output() method, except
+        # that it doesn’t have a response associated.
+
+        # Must return only requests (not items).
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class PkulawDownloaderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        # Called for each request that goes through the downloader
+        # middleware.
+
+        # Must either:
+        # - return None: continue processing this request
+        # - or return a Response object
+        # - or return a Request object
+        # - or raise IgnoreRequest: process_exception() methods of
+        #   installed downloader middleware will be called
+        return None
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class ProxyMiddleware(object):
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        return cls(
+            proxy_url=settings.get('PROXY_URL')
+        )
+
+    def __init__(self, proxy_url):
+        self.logger = logging.getLogger(__name__)
+        self.proxy_url = proxy_url
+
+    def get_random_proxy(self):
+        try:
+            ret = requests.get(self.proxy_url).json()
+            if ret['code'] == 0 and ret['success']:
+                return ret.get('data')[0].get('ip') + ':' + str(ret.get('data')[0].get('port'))
+            else:
+                return self.get_random_proxy()
+        except:
+            self.logger.debug('======异常重复获取代理======')
+            return self.get_random_proxy()
+
+    def process_request(self, request, spider):
+        proxy = self.get_random_proxy()
+        if proxy:
+            self.logger.debug('======' + '使用代理 ' + str(proxy) + "======")
+            request.meta['proxy'] = 'http://{proxy}'.format(proxy=proxy)
+
+    def process_response(self, request, response, spider):
+        if response.status != 200:
+            self.logger.debug('======返回获取代理======')
+            request.meta['proxy'] = 'http://{proxy}'.format(proxy=self.get_random_proxy())
+            return request
+        return response
+
+
+class RotateUserAgentMiddleware(UserAgentMiddleware):
+    def __init__(self, user_agent=''):
+        self.logger = logging.getLogger(__name__)
+        self.user_agent = user_agent
+
+    def process_request(self, request, spider):
+        fake_ua = UserAgent()
+        ua = fake_ua.random
+        if ua:
+            self.logger.debug('======' + '使用User-Agent ' + str(ua) + "======")
+            request.headers.setdefault('User-Agent', ua)
---
+++ pkulaw/pkulaw/pkulaw/spiders/chl_law_xa01.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020-02-04 23:41
+# @Author  : liupan
+# @Site    : 
+# @File    : chl_law_xa01.py
+# @Software: PyCharm
+
+"""
+    scrapy初始Url的两种写法，
+    一种是常量start_urls，并且需要定义一个方法parse（）
+    另一种是直接定义一个方法：star_requests()
+"""
+import re
+
+import scrapy
+from fake_useragent import UserAgent
+
+clusterwhere = '%25e6%2595%2588%25e5%258a%259b%25e7%25ba%25a7%25e5%2588%25ab%253dXA01'
+db = 'chl'
+menu_item = 'law'
+referer = 'http://www.pkulaw.cn/cluster_form.aspx?Db=chl&menu_item=law&EncodingName=&keyword=&range=name&'
+
+pattern_article = re.compile(u'^http://www.pkulaw.cn/fulltext_form.aspx\?.+$')
+
+
+class ChlLawXa01(scrapy.Spider):
+    name = 'chlLawXa01'
+
+    # start_urls = [  # 另外一种写法，无需定义start_requests方法
+    #     'http://lab.scrapyd.cn/page/1/',
+    #     'http://lab.scrapyd.cn/page/2/',
+    # ]
+
+    # 另外一种初始链接写法
+    def start_requests(self):
+        url = 'http://www.pkulaw.cn/doSearch.ashx'
+        data = {
+            'Db': db,
+            'clusterwhere': clusterwhere,
+            'clust_db': db,
+            'range': 'name',
+            'menu_item': menu_item
+        }
+        fake_ua = UserAgent()
+        headers = {
+            'User-Agent': fake_ua.random,
+            'Origin': 'http://www.pkulaw.cn',
+            'Referer': referer,
+            'Host': 'www.pkulaw.cn',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+        cookies = {
+            'isCheck': 'ValidateSuccess_126',
+            'codeCompare': 'OK_126'
+        }
+        yield scrapy.FormRequest(url=url, method='POST', headers=headers, cookies=cookies, formdata=data,
+                                 callback=self.parse, dont_filter=True)
+
+    # 如果是简写初始url，此方法名必须为：parse
+    def parse(self, response):
+        href = response.xpath('//a[@class="main-ljwenzi"]//@href').extract_first()
+        href = response.urljoin(href)
+        if re.match(pattern_article, href):
+            title = response.xpath('//a[@class="main-ljwenzi"]/text()').extract()
+            print(title)
