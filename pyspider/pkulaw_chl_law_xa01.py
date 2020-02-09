79bcf74af65b510e3ee4262ab198acadeac07e5c
liupan <1206284818@qq.com>
2020-02-09 19:49:11 +0800

    优化：去掉代理，字典get，数据库保存异常handle等

--- pyspider/pkulaw_chl_law_xa01.py
+++ pyspider/pkulaw_chl_law_xa01.py
@@ -3,9 +3,8 @@
 # Created on 2020-01-14 15:19:11
 # Project: pkulaw_chl_law_xa01 中央法规-法律
 
+import json
 import re
-import uuid
-import requests
 
 import pymysql
 from fake_useragent import UserAgent
@@ -14,45 +13,73 @@ from pyspider.libs.base_handler import *
 # 正则表达式
 pattern_article = re.compile(u'^http://www.pkulaw.cn/fulltext_form.aspx\?.+$')
 pattern_page = re.compile(u'^.*第\s+(\d+)\s+.*共\s+(\d+)\s+.*$')
-fake_ua = UserAgent()
 
 # 超时设置
-connect_timeout = 20
-retries = 3
-timeout = 120
+# connect_timeout = 20
+# retries = 3
+# timeout = 120
 # 请求参数设置
 clusterwhere = '%25e6%2595%2588%25e5%258a%259b%25e7%25ba%25a7%25e5%2588%25ab%253dXA01'
-db = 'chl'
+Db = 'chl'
+clust_db = 'chl'
 menu_item = 'law'
 referer = 'http://www.pkulaw.cn/cluster_form.aspx?Db=chl&menu_item=law&EncodingName=&keyword=&range=name&'
+
+common_formdata = {
+    'Db': Db,
+    'clust_db': clust_db,
+    'menu_item': menu_item,
+    'range': 'name',
+    'clusterwhere': clusterwhere
+}
+
+common_headers = {
+    'Referer': referer,
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'www.pkulaw.cn',
+    'Origin': 'http://www.pkulaw.cn'
+}
+common_detail_headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Host': 'www.pkulaw.cn',
+    'Upgrade-Insecure-Requests': '1'
+}
 
 
 class Handler(BaseHandler):
     crawl_config = {
-        'headers': {
-            'User-Agent': fake_ua.random,
-            'Origin': 'http://www.pkulaw.cn',
-            'Referer': referer,
-            'Host': 'www.pkulaw.cn',
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'X-Requested-With': 'XMLHttpRequest'
-        },
         'cookies': {
             'isCheck': 'ValidateSuccess_126',
             'codeCompare': 'OK_126'
         },
-        'connect_timeout': connect_timeout,
-        'retries': retries,
-        'timeout': timeout
+        # 'connect_timeout': connect_timeout,
+        # 'retries': retries,
+        # 'timeout': timeout
     }
 
-    def get_proxy(self):
-        # return requests.get("http://127.0.0.1:5010/get/").json()
-        ret = requests.get('http://http.tiqu.alicdns.com/getip3?num=1&type=2&pro=&city=0&yys=0&port=1&pack=81985&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=&gm=4').json()
-        return ret.get('data')[0].get('ip')+':'+str(ret.get('data')[0].get('port'))
+    def __init__(self):
+        self.conn = pymysql.connect(host='localhost', user='root', password='Asdf@123', port=3306, db='pkulaw')
+        self.cursor = self.conn.cursor()
+
+    def __del__(self):
+        self.conn.close()
+
+    def get_taskid(self, task):
+        return md5string(task['url']) + json.dumps(task['fetch'].get('data', ''))
+
+    # def get_proxy(self):
+    #     # return requests.get("http://127.0.0.1:5010/get/").json()
+    #     ret = requests.get(
+    #         'http://http.tiqu.alicdns.com/getip3?num=1&type=2&pro=&city=0&yys=0&port=1&pack=81985&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=&gm=4').json()
+    #     return ret.get('data')[0].get('ip') + ':' + str(ret.get('data')[0].get('port'))
 
     # def delete_proxy(self, proxy):
     #     requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy))
@@ -67,47 +94,32 @@ class Handler(BaseHandler):
 
     @every(minutes=24 * 60)
     def on_start(self):
-        custom_proxy = self.get_proxy()
-        ua = UserAgent()
-        # 第一页请求抓取
-        self.crawl('http://www.pkulaw.cn/doSearch.ashx?_=4', method='POST', data={
-            'Db': db,
-            'clusterwhere': clusterwhere,
-            'clust_db': db,
-            'range': 'name',
-            'menu_item': menu_item
-        }, callback=self.index_page, user_agent=ua.random, proxy=custom_proxy, save={'proxy': custom_proxy}, connect_timeout=connect_timeout)
+        self.crawl('http://www.pkulaw.cn/doSearch.ashx', method='POST', headers=common_headers, data=common_formdata,
+                   callback=self.index_page,
+                   user_agent=UserAgent().random)
 
     @config(age=5 * 24 * 60 * 60)
     def index_page(self, response):
-        # custom_proxy = response.save['proxy']
-        custom_proxy = self.get_proxy()
         pages = response.doc('.main-top4-1 > table > tr:first-child > td > span').text()
         pages_ret = re.match(pattern_page, pages)
         if pages_ret:
             current_index = int(pages_ret.group(1))
             page_size = int(pages_ret.group(2))
             if 1 <= current_index <= page_size:
-                # 回调index_page
-                ua = UserAgent()
-                self.crawl('http://www.pkulaw.cn/doSearch.ashx?_='+str(uuid.uuid4()), method='POST', data={
-                    'range': 'name',
-                    'Db': db,
-                    'clusterwhere': clusterwhere,
-                    'aim_page': current_index,
-                    'page_count': page_size,
-                    'clust_db': db,
-                    'menu_item': menu_item
-                }, callback=self.index_page, user_agent=ua.random, proxy=custom_proxy, save={'proxy': custom_proxy}, connect_timeout=connect_timeout)
+                next_form_data = common_formdata.copy()
+                next_form_data['aim_page'] = str(current_index)
+                next_form_data['page_count'] = str(page_size)
+                self.crawl('http://www.pkulaw.cn/doSearch.ashx', method='POST', data=next_form_data,
+                           callback=self.index_page, user_agent=UserAgent().random)
         # 逐条处理
         self.item_page(response)
 
     def item_page(self, response):
-        custom_proxy = self.get_proxy()
+        ua = UserAgent()
         for each in response.doc('a[href^="http"]').items():
             if each.attr['class'] == 'main-ljwenzi' and re.match(pattern_article, each.attr.href):
-                ua = UserAgent()
-                self.crawl(each.attr.href, callback=self.detail_page, user_agent=ua.random, proxy=custom_proxy, save={'proxy': custom_proxy}, connect_timeout=connect_timeout)
+                self.crawl(each.attr.href, headers=common_detail_headers, callback=self.detail_page,
+                           user_agent=ua.random)
 
     @config(priority=2)
     def detail_page(self, response):
@@ -139,7 +151,7 @@ class Handler(BaseHandler):
                         ret['law_type'] = td('a').text().strip()
                     elif u'类别' in strong:
                         ret['type'] = td('a').text().strip()
-                    elif u'截止日期' in strong.strip():
+                    elif u'截止日期' in strong:
                         td.children().remove()
                         ret['deadline'] = td.text().strip()
 
@@ -149,28 +161,31 @@ class Handler(BaseHandler):
         ret['content'] = main_content.strip()
 
         # 保存mysql
-        if (u'现行有效' in ret['time_valid'] or u'尚未生效' in ret['time_valid']) and (u'任免' not in ret['force_level'] and
-            u'工作文件' not in ret['force_level'] and u'工作答复' not in ret['force_level']):
-            if 'deadline' not in ret:
-                ret['deadline'] = ''
-            if 'type' not in ret:
-                ret['type'] = ''
-            if 'pub_no' not in ret:
-                ret['pub_no'] = ''
-            self.save_to_mysql(
-                (ret['title'], ret['pub_dept'], ret['pub_no'], ret['pub_date'], ret['law_type'], ret['force_level'],
-                 ret['time_valid'], ret['impl_date'], ret['content'], ret['url'], ret['type'], ret['deadline']))
+        # 1. 时效性（是）：现行有效、尚未生效
+        # 2. 效力级别（不是）：任免、工作文件、工作答复、部门工作文件、行政许可批复
+        # 3. 具有截止日期字段：立法背景资料
+        tmp_time_valid = ret.get('time_valid', '')
+        tmp_deadline = ret.get('deadline', False)
+        tmp_force_level = ret.get('force_level', '')
+        if (u'现行有效' in tmp_time_valid or u'尚未生效' in tmp_time_valid or tmp_deadline) and (
+                u'任免' not in tmp_force_level and
+                u'工作文件' not in tmp_force_level and
+                u'工作答复' not in tmp_force_level and
+                u'部门工作文件' not in tmp_force_level and
+                u'行政许可批复' not in tmp_force_level):
+            self.save_to_mysql(ret)
         return ret
 
     # 保存到mysql
-    def save_to_mysql(self, params):
-        db = pymysql.connect(host='localhost', user='root', password='Asdf@123', port=3306, db='pkulaw')
-        cursor = db.cursor()
-        sql = 'INSERT INTO law(title, pub_dept, pub_no, pub_date, law_type, force_level, time_valid, impl_date, content, url, type, deadline) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-        try:
-            cursor.execute(sql, params)
-            db.commit()
-        except:
-            db.rollback()
-        cursor.close()
-        db.close()
+    def save_to_mysql(self, ret):
+        insert_sql = """
+        INSERT INTO law(title, pub_dept, pub_no, pub_date, law_type, force_level, time_valid, impl_date, content, url, type, deadline, appr_dept, appr_date) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        self.cursor.execute(insert_sql, (
+            ret.get('title', ''), ret.get('pub_dept', ''), ret.get('pub_no', ''), ret.get('pub_date', ''),
+            ret.get('law_type', ''), ret.get('force_level', ''),
+            ret.get('time_valid', ''), ret.get('impl_date', ''), ret.get('content', ''), ret.get('url', ''),
+            ret.get('type', ''), ret.get('deadline', ''),
+            ret.get('appr_dept', ''),
+            ret.get('appr_date', '')))
+        self.conn.commit()
--- pyspider/pkulaw_lar_law_gansu.py
+++ pyspider/pkulaw_lar_law_gansu.py
@@ -3,9 +3,8 @@
 # Created on 2020-01-14 15:19:11
 # Project: pkulaw_lar_law_gansu 地方法规-甘肃
 
+import json
 import re
-import uuid
-import requests
 
 import pymysql
 from fake_useragent import UserAgent
@@ -14,82 +13,92 @@ from pyspider.libs.base_handler import *
 # 正则表达式
 pattern_article = re.compile(u'^http://www.pkulaw.cn/fulltext_form.aspx\?.+$')
 pattern_page = re.compile(u'^.*第\s+(\d+)\s+.*共\s+(\d+)\s+.*$')
-fake_ua = UserAgent()
 
+# 请求参数设置
 clusterwhere = '%25e5%258f%2591%25e5%25b8%2583%25e9%2583%25a8%25e9%2597%25a8%253d826'
-db = 'lar'
+Db = 'lar'
+clust_db = 'lar'
 menu_item = 'law'
 referer = 'http://www.pkulaw.cn/cluster_form.aspx?Db=lar&menu_item=law&EncodingName=&keyword=&range=name&'
 
+common_formdata = {
+    'Db': Db,
+    'clust_db': clust_db,
+    'menu_item': menu_item,
+    'range': 'name',
+    'clusterwhere': clusterwhere
+}
+
+common_headers = {
+    'Referer': referer,
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'www.pkulaw.cn',
+    'Origin': 'http://www.pkulaw.cn'
+}
+common_detail_headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Host': 'www.pkulaw.cn',
+    'Upgrade-Insecure-Requests': '1'
+}
+
+
 class Handler(BaseHandler):
     crawl_config = {
-        'headers': {
-            'User-Agent': fake_ua.random,
-            'Origin': 'http://www.pkulaw.cn',
-            'Referer': referer,
-            'Host': 'www.pkulaw.cn',
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'X-Requested-With': 'XMLHttpRequest',
-            'Connection': 'keep-alive'
-        },
         'cookies': {
             'isCheck': 'ValidateSuccess_126',
             'codeCompare': 'OK_126'
         }
     }
 
-    def get_proxy(self):
-        ret = requests.get(
-            'http://http.tiqu.alicdns.com/getip3?num=1&type=2&pro=&city=0&yys=0&port=1&pack=81985&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=&gm=4').json()
-        return ret.get('data')[0].get('ip') + ':' + str(ret.get('data')[0].get('port'))
+    def __init__(self):
+        self.conn = pymysql.connect(host='localhost', user='root', password='Asdf@123', port=3306, db='pkulaw')
+        self.cursor = self.conn.cursor()
+
+    def __del__(self):
+        self.conn.close()
+
+    def get_taskid(self, task):
+        return md5string(task['url']) + json.dumps(task['fetch'].get('data', ''))
 
     @every(minutes=24 * 60)
     def on_start(self):
-        ua = UserAgent()
-        # 第一页请求抓取
-        self.crawl('http://www.pkulaw.cn/doSearch.ashx?_=1', method='POST', data={
-            'Db': db,
-            'clusterwhere': clusterwhere,
-            'clust_db': db,
-            'range': 'name',
-            'menu_item': menu_item
-        }, callback=self.index_page, user_agent=ua.random, proxy=self.get_proxy())
+        self.crawl('http://www.pkulaw.cn/doSearch.ashx', method='POST', headers=common_headers, data=common_formdata,
+                   callback=self.index_page,
+                   user_agent=UserAgent().random)
 
-    @config(age=10 * 24 * 60 * 60)
+    @config(age=5 * 24 * 60 * 60)
     def index_page(self, response):
-        proxy = self.get_proxy()
         pages = response.doc('.main-top4-1 > table > tr:first-child > td > span').text()
         pages_ret = re.match(pattern_page, pages)
         if pages_ret:
             current_index = int(pages_ret.group(1))
             page_size = int(pages_ret.group(2))
             if 1 <= current_index <= page_size:
-                ua = UserAgent()
-                self.crawl('http://www.pkulaw.cn/doSearch.ashx?_=' + str(uuid.uuid4()), method='POST', data={
-                    'range': 'name',
-                    'Db': db,
-                    'clusterwhere': clusterwhere,
-                    'aim_page': current_index,
-                    'page_count': page_size,
-                    'clust_db': db,
-                    'menu_item': menu_item
-                }, callback=self.index_page, user_agent=ua.random, proxy=proxy)
+                next_form_data = common_formdata.copy()
+                next_form_data['aim_page'] = str(current_index)
+                next_form_data['page_count'] = str(page_size)
+                self.crawl('http://www.pkulaw.cn/doSearch.ashx', method='POST', data=next_form_data,
+                           callback=self.index_page, user_agent=UserAgent().random)
         # 逐条处理
         self.item_page(response)
 
     def item_page(self, response):
-        proxy = self.get_proxy()
+        ua = UserAgent()
         for each in response.doc('a[href^="http"]').items():
             if each.attr['class'] == 'main-ljwenzi' and re.match(pattern_article, each.attr.href):
-                ua = UserAgent()
-                self.crawl(each.attr.href, callback=self.detail_page, user_agent=ua.random, proxy=proxy)
+                self.crawl(each.attr.href, headers=common_detail_headers, callback=self.detail_page,
+                           user_agent=ua.random)
 
     @config(priority=2)
     def detail_page(self, response):
-        # 详细处理
         title = response.doc('table#tbl_content_main > tr:first-child > td > span > strong')
         li_list = response.doc('table#tbl_content_main > tr').items()
         ret = {}
@@ -101,11 +110,6 @@ class Handler(BaseHandler):
                     strong = strong.strip()
                     if u'发布部门' in strong:
                         ret['pub_dept'] = td('a').text().strip()
-                    elif u'批准部门' in strong:
-                        ret['appr_dept'] = td('a').text().strip()
-                    elif u'批准日期' in strong:
-                        td.children().remove()
-                        ret['appr_date'] = td.text().strip()
                     elif u'发文字号' in strong:
                         td.children().remove()
                         ret['pub_no'] = td.text().strip()
@@ -129,34 +133,38 @@ class Handler(BaseHandler):
 
         ret['url'] = response.url
         ret['title'] = title.text().strip()
-        ret['content'] = response.doc('.Content > #div_content').html().strip()
+        main_content = response.doc('.Content > #div_content').html()
+        ret['content'] = main_content.strip()
 
         # 保存mysql
-        if u'现行有效' in ret['time_valid'] or u'尚未生效' in ret['time_valid']:
-            if 'appr_dept' not in ret:
-                ret['appr_dept'] = ''
-            if 'appr_date' not in ret:
-                ret['appr_date'] = ''
-            if 'deadline' not in ret:
-                ret['deadline'] = ''
-            if 'type' not in ret:
-                ret['type'] = ''
-            if 'pub_no' not in ret:
-                ret['pub_no'] = ''
-            self.save_to_mysql(
-                (ret['title'], ret['pub_dept'], ret['pub_no'], ret['pub_date'], ret['law_type'], ret['force_level'],
-                 ret['time_valid'], ret['impl_date'], ret['content'], ret['url'], ret['type'], ret['deadline'], ret['appr_dept'], ret['appr_date']))
+        # 1. 时效性（是）：现行有效、尚未生效
+        # 2. 效力级别（不是）：任免、工作文件、工作答复、部门工作文件、行政许可批复
+        # 3. 具有截止日期字段：立法背景资料
+        tmp_time_valid = ret.get('time_valid', '')
+        tmp_deadline = ret.get('deadline', False)
+        tmp_force_level = ret.get('force_level', '')
+        if (u'现行有效' in tmp_time_valid or u'尚未生效' in tmp_time_valid or tmp_deadline) and (
+                u'任免' not in tmp_force_level and
+                u'工作文件' not in tmp_force_level and
+                u'工作答复' not in tmp_force_level and
+                u'部门工作文件' not in tmp_force_level and
+                u'行政许可批复' not in tmp_force_level):
+            self.save_to_mysql(ret)
         return ret
 
     # 保存到mysql
-    def save_to_mysql(self, params):
-        db = pymysql.connect(host='localhost', user='root', password='Asdf@123', port=3306, db='pkulaw')
-        cursor = db.cursor()
-        sql = 'INSERT INTO law(title, pub_dept, pub_no, pub_date, law_type, force_level, time_valid, impl_date, content, url, type, deadline, appr_dept, appr_date) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    def save_to_mysql(self, ret):
+        insert_sql = """
+        INSERT INTO law(title, pub_dept, pub_no, pub_date, law_type, force_level, time_valid, impl_date, content, url, type, deadline, appr_dept, appr_date) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
         try:
-            cursor.execute(sql, params)
-            db.commit()
-        except:
-            db.rollback()
-        cursor.close()
-        db.close()
+            self.cursor.execute(insert_sql, (
+                ret.get('title', ''), ret.get('pub_dept', ''), ret.get('pub_no', ''), ret.get('pub_date', ''),
+                ret.get('law_type', ''), ret.get('force_level', ''),
+                ret.get('time_valid', ''), ret.get('impl_date', ''), ret.get('content', ''), ret.get('url', ''),
+                ret.get('type', ''), ret.get('deadline', ''),
+                ret.get('appr_dept', ''),
+                ret.get('appr_date', '')))
+            self.conn.commit()
+        except pymysql.err.IntegrityError:
+            print('Repeat Key')
