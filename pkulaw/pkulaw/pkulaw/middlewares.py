8ff05092e4f930f5d25b239cb59e229aa5a8171b
liupan <1206284818@qq.com>
2020-02-24 08:23:20 +0800

    阿布云代理

--- pkulaw/pkulaw/pkulaw/middlewares.py
+++ pkulaw/pkulaw/pkulaw/middlewares.py
@@ -5,115 +5,41 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+import base64
 import logging
 import re
 
 import requests
 from pkulaw.ua.user_agent import UserAgent
 from pkulaw.utils import is_expired
-from scrapy import signals
 
 logger = logging.getLogger(__name__)
 
 
-class PkulawSpiderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
+class AbuyunProxyMiddleware(object):
+    """
+    阿布云代理中间件
+    """
 
     @classmethod
     def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+        return cls(crawler.settings)
 
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Request, dict
-        # or Item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
-
-
-class PkulawDownloaderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+    def __init__(self, settings):
+        self.abuyun_proxy_server = settings.get('ABUYUN_PROXY_SERVER')
+        abuyun_proxy_user = settings.get('ABUYUN_PROXY_USER')
+        abuyun_proxy_pass = settings.get('ABUYUN_PROXY_PASS')
+        self.abuyun_proxy_auth = "Basic " + base64.urlsafe_b64encode(
+            bytes((abuyun_proxy_user + ":" + abuyun_proxy_pass), "ascii")).decode("utf8")
 
     def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
-
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
-        return None
-
-    def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
-
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
-
-    def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
-
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
-        pass
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        request.meta["proxy"] = self.abuyun_proxy_server
+        request.headers["Proxy-Authorization"] = self.abuyun_proxy_auth
 
 
-class ProxyMiddleware(object):
+class ZhimaProxyMiddleware(object):
     """
-    代理中间件
+    芝麻代理中间件
     """
 
     @classmethod
@@ -124,7 +50,7 @@ class ProxyMiddleware(object):
         self.max_retry_times = settings.getint('RETRY_TIMES')
         self.retry_http_codes = set(int(x) for x in settings.getlist('RETRY_HTTP_CODES'))
         self.start_url = settings.get('START_URL')
-        self.proxy_url = settings.get('PROXY_URL')
+        self.proxy_url = settings.get('ZHIMA_PROXY_URL')
         self.proxy_list = self.get_proxy_list()
         self.pattern_article = re.compile(u'^http://www.pkulaw.cn/fulltext_form.aspx\?.+$')
 
@@ -153,7 +79,8 @@ class ProxyMiddleware(object):
         if (url == self.start_url or re.match(self.pattern_article, url)) and response.status != 200:
             logger.error('############status: ' + str(response.status) + '###############')
             logger.error('############flags: ' + str(response.flags) + '###############')
-            logger.error('############body: ' + str(response.body.decode('utf8') if response.body else response.body) + '###############')
+            logger.error('############body: ' + str(
+                response.body.decode('utf8') if response.body else response.body) + '###############')
             # 1. IP被封了，更新代理
             if response.status == 403:
                 self.update_proxy(request)
@@ -205,7 +132,7 @@ class RandomUserAgentMiddleware(object):
 #
 #     def __init__(self, settings):
 #         super().__init__(settings)
-#         self.proxy_url = settings.get('PROXY_URL')
+#         self.proxy_url = settings.get('ZHIMA_PROXY_URL')
 #
 #     def get_proxy_list(self):
 #         return requests.get(self.proxy_url).json()
